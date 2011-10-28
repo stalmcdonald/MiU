d22e9db09a10164a18c6ef2a12130b09e44bec48
Crystal McDonald <stalmcdonald@fullsail.edu>
2011-10-27 19:07:26 -0700

    Commit for project 1
    Entire App complete

---
+++ mcdonald_crystal_project1/js/index.js
@@ -0,0 +1,12 @@
+  
+  
+  
+  
+  
+  	$("#accordion > li > div").click(function(){
+   		if(false == $(this).next().is(':visible')) {$('#accordion ul').slideUp(300);}
+    	$(this).next().slideToggle(300);
+	});
+ 
+		$('#accordion ul:eq(0)').show();  //To make one open by default
+ 
